d1f0f5f2c38a1f8988b6302ebf76a6b0b2bad8eb
Mauro Lepore <maurolepore@gmail.com>
2023-12-08 22:11:28 +0000

    Run tilt profiles by chunk

--- R/tiltWorkflows-package.R
+++ R/tiltWorkflows-package.R
@@ -2,6 +2,23 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr .data
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr row_number
+#' @importFrom dplyr select
+#' @importFrom fs dir_create
+#' @importFrom fs file_exists
+#' @importFrom fs path
+#' @importFrom furrr future_pwalk
+#' @importFrom future availableWorkers
+#' @importFrom purrr map_df
+#' @importFrom rappdirs user_cache_dir
+#' @importFrom readr read_rds
+#' @importFrom readr write_rds
+#' @importFrom tidyr nest
+#' @importFrom tidyr unnest
+#' @importFrom tidyselect all_of
 #' @importFrom tiltIndicatorAfter unnest_product
 #' @importFrom tiltToyData toy_files
 ## usethis namespace: end
---
+++ R/utils-profile.R
@@ -0,0 +1,87 @@
+profile <- function(companies, .f, ..., .by, chunks) {
+  job <- companies |>
+    nest_chunk(.by = .by, chunks = chunks) |>
+    add_file(parent = cache_path(deparse(substitute(.f))))
+
+  job |>
+    job_pmap(\(data, file) .f(data, ...) |> write_rds(file))
+}
+#' Run a `profile*()` function by chunks
+#'
+#' @param companies Data frame.
+#' @param .f A `profile*()` function from tiltIndicatorAfter.
+#' @param ... Arguments passed to `.f`.
+#' @param .by Character. Columns to chunk by.
+#' @param chunks Integer. Number of chunks to split the data by.
+#'
+#' @return A nested data frame with results at product and company level.
+#' @export
+#' @keywords internal
+#'
+#' @examples
+#' library(tiltToyData)
+#' library(readr, warn.conflicts = FALSE)
+#'
+#' options(readr.show_col_types = FALSE)
+#'
+#' companies <- read_csv(toy_emissions_profile_any_companies())
+#' products <- read_csv(toy_emissions_profile_products())
+#'
+#' result <- companies |>
+#'   profile_with(profile_emissions,
+#'     products,
+#'     # TODO: Move to tiltToyData
+#'     europages_companies = tiltIndicatorAfter::ep_companies,
+#'     ecoinvent_activities = tiltIndicatorAfter::ecoinvent_activities,
+#'     ecoinvent_europages = tiltIndicatorAfter::matches_mapper |> head(100),
+#'     isic_tilt = tiltIndicatorAfter::isic_tilt_mapper
+#'   )
+#'
+#' result |> unnest_product()
+#'
+#' result |> unnest_company()
+profile_with <- purrr::partial(
+  profile,
+  .by = "companies_id",
+  chunks = getOption("tiltWorkflow.chunks", default = length(availableWorkers()))
+)
+
+cache_path <- function(..., parent = cache_dir()) {
+  path(parent, ...)
+}
+
+cache_dir <- function() {
+  user_cache_dir(appname = "tiltIndicator")
+}
+
+job_pmap <- function(job, .f) {
+  job |>
+    pick_undone() |>
+    select(.data$data, .data$file) |>
+    future_pwalk(.f, .progress = TRUE)
+
+  map_df(job$file, read_rds)
+}
+
+nest_chunk <- function(data, .by, chunks) {
+  data |>
+    nest(.by = all_of(.by)) |>
+    mutate(data, chunk = as.integer(cut(row_number(), chunks))) |>
+    unnest(data) |>
+    nest(.by = .data$chunk)
+}
+
+add_file <- function(data, parent = cache_path(), ext = ".rds") {
+  dir_create(parent)
+  mutate(data, file = path(parent, paste0(.data$chunk, ext)))
+}
+
+pick_undone <- function(data) {
+  data |>
+    add_done() |>
+    filter(!.data$done)
+}
+
+add_done <- function(data, file) {
+  mutate(data, done = file_exists(file))
+}
