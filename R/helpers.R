ea9626b892aa8b250401c71a8d417924e6cce858
Mauro Lepore <maurolepore@gmail.com>
2023-12-09 19:18:34 +0000

    wip

---
+++ R/helpers.R
@@ -0,0 +1,32 @@
+#' Create a path under ~/Downwloads/tilt/
+#'
+#' @inherit fs::path_home
+#'
+#' @return Character. A path.
+#' @export
+#'
+#' @examples
+#' tilt_path()
+tilt_path <- function(...) {
+  path_home("Downloads", "tilt", ...)
+}
+
+#' Read a .csv but if it doesnt' exist fall back to another dataset
+#'
+#' @param path Character. Path to a .csv file.
+#' @param data Data frame. An alternative dataset to fall back to if the `path`
+#'   points to a file that doesn't exist.
+#'
+#' @return A data frame.
+#' @export
+#'
+#' @examples
+#' read_csv_falling_back_to("/inexistent/file.csv", head(cars))
+read_csv_falling_back_to <- function(path, data) {
+  if (file_exists(path)) {
+    read_csv(path)
+  } else {
+    warning("Using ", deparse(substitute(data)), call. = FALSE)
+    data
+  }
+}
