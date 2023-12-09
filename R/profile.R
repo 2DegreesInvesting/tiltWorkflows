76366cf4e00fccc7a73c0232e1ce8f5c639872cf
Mauro Lepore <maurolepore@gmail.com>
2023-12-09 19:16:26 +0000

    wip

--- R/profile.R
+++ R/profile.R
@@ -15,7 +15,7 @@ profile_emissions <- function(companies,
       europages_companies = europages_companies,
       ecoinvent_activities = ecoinvent_activities,
       ecoinvent_europages = ecoinvent_europages,
-      isic = isic,
+      isic_tilt = isic_tilt,
       low_threshold = 1 / 3,
       high_threshold = 2 / 3
     )
@@ -40,7 +40,7 @@ profile_emissions_upstream <- function(companies,
       ecoinvent_activities = ecoinvent_activities,
       ecoinvent_inputs = ecoinvent_inputs,
       ecoinvent_europages = ecoinvent_europages,
-      isic = isic,
+      isic_tilt = isic_tilt,
       low_threshold = 1 / 3,
       high_threshold = 2 / 3
     )
@@ -48,7 +48,7 @@ profile_emissions_upstream <- function(companies,
 
 #' @export
 #' @keywords internal
-#' @inherit tiltIndicatorAfter::profile_emissions_upstream
+#' @inherit tiltIndicatorAfter::profile_sector
 profile_sector <- function(companies,
                            scenarios,
                            europages_companies,
@@ -66,5 +66,32 @@ profile_sector <- function(companies,
       isic_tilt = isic_tilt,
       low_threshold = low_threshold,
       high_threshold = high_threshold
+    )
+}
+
+#' @export
+#' @keywords internal
+#' @inherit tiltIndicatorAfter::profile_sector_upstream
+profile_sector_upstream <- function(companies,
+                                    scenarios,
+                                    inputs,
+                                    europages_companies,
+                                    ecoinvent_activities,
+                                    ecoinvent_inputs,
+                                    ecoinvent_europages,
+                                    isic_tilt,
+                                    low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
+                                    high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
+  companies |>
+    profile_with(tiltIndicatorAfter::profile_sector_upstream,
+      scenarios = scenarios,
+      inputs = inputs,
+      europages_companies = europages_companies,
+      ecoinvent_activities = ecoinvent_activities,
+      ecoinvent_inputs = ecoinvent_inputs,
+      ecoinvent_europages = ecoinvent_europages,
+      isic_tilt = isic_tilt,
+      low_threshold = 1 / 3,
+      high_threshold = 2 / 3
     )
 }
