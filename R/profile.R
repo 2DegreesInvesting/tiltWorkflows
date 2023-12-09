1f5b3a3ea818843f86333c99011198ab27f094b0
Mauro Lepore <maurolepore@gmail.com>
2023-12-09 18:46:51 +0000

    wip

--- R/profile.R
+++ R/profile.R
@@ -11,12 +11,34 @@ profile_emissions <- function(companies,
                               high_threshold = 2 / 3) {
   companies |>
     profile_with(tiltIndicatorAfter::profile_emissions,
-                 co2 = co2,
-                 europages_companies = europages_companies,
-                 ecoinvent_activities = ecoinvent_activities,
-                 ecoinvent_europages = ecoinvent_europages,
-                 isic = isic,
-                 low_threshold = 1 / 3,
-                 high_threshold = 2 / 3
+      co2 = co2,
+      europages_companies = europages_companies,
+      ecoinvent_activities = ecoinvent_activities,
+      ecoinvent_europages = ecoinvent_europages,
+      isic = isic,
+      low_threshold = 1 / 3,
+      high_threshold = 2 / 3
+    )
+}
+
+profile_emissions_upstream <- function(companies,
+                                       co2,
+                                       europages_companies,
+                                       ecoinvent_activities,
+                                       ecoinvent_inputs,
+                                       ecoinvent_europages,
+                                       isic_tilt,
+                                       low_threshold = 1 / 3,
+                                       high_threshold = 2 / 3) {
+  companies |>
+    profile_with(tiltIndicatorAfter::profile_emissions_upstream,
+      co2 = co2,
+      europages_companies = europages_companies,
+      ecoinvent_activities = ecoinvent_activities,
+      ecoinvent_inputs = ecoinvent_inputs,
+      ecoinvent_europages = ecoinvent_europages,
+      isic = isic,
+      low_threshold = 1 / 3,
+      high_threshold = 2 / 3
     )
 }
