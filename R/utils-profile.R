95c98dc9e6c7418574c91f3cc7df1a1d80c3d0bc
Mauro Lepore <maurolepore@gmail.com>
2023-12-09 18:43:14 +0000

    refactor

--- R/utils-profile.R
+++ R/utils-profile.R
@@ -85,26 +85,3 @@ pick_undone <- function(data) {
 add_done <- function(data, file) {
   mutate(data, done = file_exists(file))
 }
-
-#' @export
-#' @keywords internal
-#' @inherit tiltIndicatorAfter::profile_emissions
-profile_emissions <- function(companies,
-                              co2,
-                              europages_companies,
-                              ecoinvent_activities,
-                              ecoinvent_europages,
-                              isic_tilt,
-                              low_threshold = 1 / 3,
-                              high_threshold = 2 / 3) {
-  companies |>
-    profile_with(tiltIndicatorAfter::profile_emissions,
-      co2 = co2,
-      europages_companies = europages_companies,
-      ecoinvent_activities = ecoinvent_activities,
-      ecoinvent_europages = ecoinvent_europages,
-      isic = isic,
-      low_threshold = 1 / 3,
-      high_threshold = 2 / 3
-    )
-}
