e90efc895d99f2ce189ef48c3bb52baf4dccb48f
Mauro Lepore <maurolepore@gmail.com>
2023-12-12 08:59:49 +0000

    Refactor

--- R/use_workflow.R
+++ R/use_workflow.R
@@ -9,12 +9,12 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Note: Running this will write "tilt-profiles.Rmd" to your working directory
+#' # Note: Running this will write "profile.Rmd" to your working directory
 #' use_workflow()
 #' # Same
-#' use_workflow("tilt-profiles.Rmd")
+#' use_workflow("profile.Rmd")
 #' }
-use_workflow <- function(template = "tilt-profiles.Rmd",
+use_workflow <- function(template = "profile.Rmd",
                          save_as = template,
                          open = FALSE) {
   usethis::use_template(
