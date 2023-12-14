3db758124fde8c68a197052a773970002b81b403
Mauro Lepore <maurolepore@gmail.com>
2023-12-14 07:08:29 +0000

    If 'template' is missing throw an error

--- tests/testthat/test-use_workflow.R
+++ tests/testthat/test-use_workflow.R
@@ -1,4 +1,4 @@
-test_that("defaults to tiltWorkflows.Rmd", {
+test_that("writes a workflow", {
   skip_if_R_CMD_check()
 
   create_local_project()
@@ -13,4 +13,8 @@ test_that("is sensitive to `save_as`", {
   file <- "custom.Rmd"
   suppressMessages(use_workflow("profile_emissions.Rmd", save_as = file))
   expect_true(file.exists(file))
+})
+
+test_that("without a workflow errors gracefully", {
+  expect_snapshot_error(use_workflow())
 })
