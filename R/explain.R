122bc1c67dfbec4eebc47b118f89f23c3d3f0f67
Mauro Lepore <maurolepore@gmail.com>
2023-12-15 14:24:55 +0000

    document

--- R/explain.R
+++ R/explain.R
@@ -1,7 +1,17 @@
+#' Explain sections consistently accross workflows.
+#'
+#' @return Character.
+#' @export
+#' @keywords internal
+#'
+#' @examples
+#' explain_indicator()
 explain_indicator <- function() {
   "Compute the resutls for this specific indicator."
 }
 
+#' @export
+#' @rdname explain_indicator
 explain_resutls <- function() {
   "See and save the results at product and company level."
 }
