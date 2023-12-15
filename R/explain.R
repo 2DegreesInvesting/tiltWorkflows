ff1b51055cfd4800f18b47077cb14f99bc97480c
Mauro Lepore <maurolepore@gmail.com>
2023-12-15 14:22:40 +0000

    Explain sections

---
+++ R/explain.R
@@ -0,0 +1,7 @@
+explain_indicator <- function() {
+  "Compute the resutls for this specific indicator."
+}
+
+explain_resutls <- function() {
+  "See and save the results at product and company level."
+}
