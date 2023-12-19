60af8a6d761befe662225d19681ba33d5c59327e
Mauro Lepore <maurolepore@gmail.com>
2023-12-19 18:15:52 +0000

    wip

--- R/utils-devel.R
+++ R/utils-devel.R
@@ -118,5 +118,5 @@ cache_info <- function(cache_dir = default_cache_dir()) {
   fs::dir_ls(cache_dir, recurse = TRUE, type = "file") |>
     fs::file_info() |>
     dplyr::arrange(.data$modification_time) |>
-    dplyr::relocate(.data$modification_time, path)
+    dplyr::relocate("modification_time", path)
 }
