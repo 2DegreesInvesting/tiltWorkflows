5ace8d55bf134766c22399ff11c8b21c6e3ffab2
Mauro Lepore <maurolepore@gmail.com>
2023-12-21 18:05:54 +0000

    * Ensure chunks is numeric (#106)

--- tests/testthat/test-profile.R
+++ tests/testthat/test-profile.R
@@ -63,6 +63,29 @@ test_that("outputs the same as tiltIndicatorAfter", {
   expect_equal(masked, original)
 })
 
+test_that("with `chunks` passed as a character throws no error", {
+  withr::local_options(list(
+    readr.show_col_types = FALSE,
+    tiltWorkflows.chunks = as.character(3),
+    tiltWorkflows.cache_dir = withr::local_tempdir()
+  ))
+
+  companies <- read_csv(toy_emissions_profile_any_companies())
+  products <- read_csv(toy_emissions_profile_products())
+
+  expect_no_error(
+    profile_emissions(
+      companies,
+      products,
+      # TODO: Move to tiltToyData
+      europages_companies = tiltIndicatorAfter::ep_companies,
+      ecoinvent_activities = tiltIndicatorAfter::ecoinvent_activities,
+      ecoinvent_europages = tiltIndicatorAfter::matches_mapper |> head(100),
+      isic_tilt = tiltIndicatorAfter::isic_tilt_mapper
+    )
+  )
+})
+
 test_that("with `tiltWorkflows.chunks = NULL` wanrs auto set chunks", {
   withr::local_options(list(
     readr.show_col_types = FALSE,
