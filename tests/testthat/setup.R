0692ca39739140005f840eece3b47f7e24b8223f
Mauro Lepore <maurolepore@gmail.com>
2024-03-09 13:54:37 -0300

    In tests DRY options via setup.R (#133)

---
+++ tests/testthat/setup.R
@@ -0,0 +1,3 @@
+# https://testthat.r-lib.org/articles/special-files.html#setup-files
+withr::local_options(readr.show_col_types = FALSE, .local_envir = teardown_env())
+withr::local_options(tiltIndicatorAfter.verbose = FALSE, .local_envir = teardown_env())
