a5562fe5365df723f58687d032ce908ab3c63040
Mauro Lepore <maurolepore@gmail.com>
2024-03-10 20:36:09 +0000

    Remove warning

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,6 @@
 extract_options <- function(pattern) {
   out <- options()[grep(pattern, names(options()), value = TRUE)]
   if (rlang::is_empty(out)) {
-    rlang::warn(c(
-      glue::glue("The pattern '{pattern}' matched no option."),
-      i = "Do you need to fix a typo?",
-      v = "Returning the original `options()`."
-    ))
     return(options())
   }
   out
